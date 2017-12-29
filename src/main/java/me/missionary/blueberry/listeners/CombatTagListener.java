557f51f90497efd9931c7202d4ef6b7d1469a644
Missionary <missionarymc@gmail.com>
2017-12-28 23:13:53 -0700

    Add a combat tag to the damager

--- src/main/java/me/missionary/blueberry/listeners/CombatTagListener.java
+++ src/main/java/me/missionary/blueberry/listeners/CombatTagListener.java
@@ -1,5 +1,6 @@
 package me.missionary.blueberry.listeners;
 
+import me.missionary.blueberry.Blueberry;
 import me.missionary.blueberry.scoreboard.Scoreboard;
 import me.missionary.blueberry.scoreboard.timer.Timer;
 import org.bukkit.entity.Arrow;
@@ -8,7 +9,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 
 /**
  * Created by Missionary (missionarymc@gmail.com) on 12/27/2017.
@@ -20,13 +20,24 @@ public class CombatTagListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onEntityDamage(EntityDamageByEntityEvent event) {
         if (event.getEntity() instanceof Player) {
+            if (Blueberry.getPlugin().getSpawnManager().contains(event.getEntity().getLocation())) {
+                return;
+            }
             if (event.getDamager() instanceof Player || (event.getDamager() instanceof Arrow && ((Arrow) event.getDamager()).getShooter() instanceof Player)) {
-                System.out.println("CALLED COMBATTAGLISTENER - ONEDE"); // TODO: 12/27/2017 GIVE THE TAGGER A TAG
+                System.out.println("CALLED COMBATTAGLISTENER - ONEDE");
                 Player player = (Player) event.getEntity();
                 if (Scoreboard.getPlayer(player).getTimer(COMBAT_TAG_KEY) == null) {
                     new Timer(Scoreboard.getPlayer(player), COMBAT_TAG_KEY, 30L);
                 } else {
                     Scoreboard.getPlayer(player).getTimer(COMBAT_TAG_KEY).setNewEnd(30L);
+                }
+                if (event.getDamager() instanceof Player) {
+                    Player damager = (Player) event.getDamager();
+                    if (Scoreboard.getPlayer(damager).getTimer(COMBAT_TAG_KEY) == null) {
+                        new Timer(Scoreboard.getPlayer(damager), COMBAT_TAG_KEY, 30L);
+                    } else {
+                        Scoreboard.getPlayer(damager).getTimer(COMBAT_TAG_KEY).setNewEnd(30L);
+                    }
                 }
             }
         }
